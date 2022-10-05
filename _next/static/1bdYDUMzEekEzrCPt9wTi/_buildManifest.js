8db7e72af99a4a5813368a507efdc8cfdc783830
weretere <38572760+weretere@users.noreply.github.com>
2022-10-05 21:04:10 +0000

    Deploying to public from @ weretere/react-resume-template@16e20849d36cef98ae924ab0c81acc3a7f8c3bb5 🚀

--- _next/static/1bdYDUMzEekEzrCPt9wTi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/126-993df1da1274f968.js","static/chunks/pages/index-3b8748d2726ba507.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/iDLEe3xZm2fS97qBIA_gG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/126-993df1da1274f968.js","static/chunks/pages/index-56151bbdad8efe70.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
