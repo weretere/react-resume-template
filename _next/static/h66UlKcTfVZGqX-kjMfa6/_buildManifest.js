bd378fc88748f2badd80c8764edea59e4d0dc3c8
weretere <38572760+weretere@users.noreply.github.com>
2022-10-05 13:45:51 +0000

    Deploying to public from @ weretere/react-resume-template@06d096dec11c1ea5ed536bfff280d6da11e43989 🚀

--- _next/static/h66UlKcTfVZGqX-kjMfa6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/249-cafd5bc4feab8ea9.js","static/chunks/pages/index-e344bdbf30a22419.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
